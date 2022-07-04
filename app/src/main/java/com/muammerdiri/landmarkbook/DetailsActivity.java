154db3061ea46229520d7f761cdd23b5b1e26219
Muammer Diri <muammerdiri2@gmail.com>
2022-07-04 12:36:38 +0300

    DetailActivity fixed

--- app/src/main/java/com/muammerdiri/landmarkbook/DetailsActivity.java
+++ app/src/main/java/com/muammerdiri/landmarkbook/DetailsActivity.java
@@ -2,13 +2,33 @@ package com.muammerdiri.landmarkbook;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.muammerdiri.landmarkbook.databinding.ActivityDetailsBinding;
+
+import java.util.ArrayList;
 
 public class DetailsActivity extends AppCompatActivity {
+
+
+    private ActivityDetailsBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_details);
+        binding = ActivityDetailsBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        Intent intent = getIntent();
+
+        Landmark selectedLandmark = (Landmark) intent.getSerializableExtra("landmark");
+        binding.name.setText(selectedLandmark.name);
+        binding.country.setText(selectedLandmark.country);
+        binding.imageView.setImageResource(selectedLandmark.image);
+
+
     }
 }
---
+++ app/src/main/java/com/muammerdiri/landmarkbook/LandmarkAdapter.java
@@ -0,0 +1,2 @@
+package com.muammerdiri.landmarkbook;public class LandmarkAdapter {
+}
